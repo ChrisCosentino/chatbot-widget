d1fda7bf3db22df103f388ea487a8024a2e0dfb0
ChrisCosentino <chriscosentino@hotmail.com>
2024-10-23 00:33:27 -0400

    intial commit

---
+++ src/chat-widget.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function ChatWidget() {
+	return (
+		<div style={{ position: 'fixed', right: 10, bottom: 10 }}>Button HEre</div>
+	);
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import r2wc from '@r2wc/react-to-web-component';
+import { ChatWidget } from './chat-widget';
+
+const wcChat = r2wc(ChatWidget, {});
+
+customElements.define('r2w-chat', wcChat);
+// import Checklist from './components/checklist/Checklist';
+
+// const wcChecklist = r2wc(Checklist, { props: { items: "json" } });
+
+// customElements.define("r2w-checklist", wcChecklist);
